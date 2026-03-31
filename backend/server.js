8818ed059ad59d5e0dbfeeb48e0deac604d89386
Jitesh7140 <jiteshsaini2005@gmail.com>
2026-03-31 11:11:37 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+ 
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use('/', (req, res) => {
+  res.send("Hello World");
+})
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
