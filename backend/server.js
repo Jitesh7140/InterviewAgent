a285e8c19fdc72de92394e110bf821dbb5f40491
Jitesh7140 <jiteshsaini2005@gmail.com>
2026-04-07 22:44:14 +0530

    allset start deploymnt

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   credentials: true,
 };
 app.use(cors(corsOptions));
