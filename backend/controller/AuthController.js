c18074512dfa694887f50713d186236622c40696
Jitesh7140 <jiteshsaini2005@gmail.com>
2026-04-07 22:17:42 +0530

    allset start deploymnt

--- backend/controller/AuthController.js
+++ backend/controller/AuthController.js
@@ -12,7 +12,7 @@ exports.googleAuth = async (req, res) => {
       res.cookie("AuthToken", token, {
         httpOnly: true,
         secure: true,
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
